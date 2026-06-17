901118b7204bab86760a0854edc87057c41a309f
misato729 <valor729@icloud.com>
2026-06-18 07:52:30 +0900

    環境構築2

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :health, only: :show
+      resource :health, only: :show, controller: :health
     end
   end
 end
